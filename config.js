d96b9d7f71850921b6bb418e2254c01f00866363
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:26:48 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' FKJ ';
+export let nameSongs = [
+	"YLANG YLANG", /*1*/
+	"SKYLINE", /*2*/
+	"TADOW ", /*3*/
+	"LOSING MY WAY", /*4*/
+	"TOKYO", /*5*/
+	"KASOY", /*6*/
+	"MONDAY JAM", /*7*/
+	"A MOMENT OF MYSTERY", /*8*/
+	"BROTHER", /*9*/
+	"ON MY OWN", /*10*/
+	"VIBIN' OUT ", /*11*/
+	"LUCKY STAR", /*12*/
+	"RISK", /*13*/
+	"10 YEARS AGO", /*14*/
+	"MOMENTS", /*15*/
+	"DOES IT EXIST", /*16*/
+	"LEAVE MY HOME", /*17*/
+	"DIE WITH A SMILE", /*18*/
+	"WHY ARE THERE BOUNDARIES", /*19*/
+	"ENVIRONMENTS CHANGE YOU", /*20*/
+]
